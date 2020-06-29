ca7e495250c5cbe74198b2aba3edfa6d8888281f
Trent Bennett <rehab.rtb@gmail.com>
2020-06-29 08:21:44 -0600

    adding the constants

---
+++ src/constants/AppActions.js
@@ -0,0 +1,8 @@
+const AppActions = {
+  FETCH_CONVERSATIONS_SUCCESS: "fetch_conversation_success",
+  FETCH_CONVERSATIONS_FAILURE: "fetch_conversation_failure",
+  POST_MESSAGE_SUCCESS: "post_message_success",
+  POST_MESSAGE_FAILURE: "post_message_failure",
+};
+
+export default AppActions;
---
+++ src/constants/URI.js
@@ -0,0 +1,8 @@
+const URI = {
+  ROOT_URL: "http://localhost:5000",
+  SEND_MESSAGE: "/message",
+  GET_CONVERSATIONS: "/messages",
+  GET_RESUMES: "/resumes",
+};
+
+export default URI;
